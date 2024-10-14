2957355c0d6242786f696ab747f37ed357bf0021
Yufei-Xiang <73530768+Yufei-Xiang@users.noreply.github.com>
2024-10-14 13:09:37 +0800

    Update iql.py

--- IQL/src/iql.py
+++ IQL/src/iql.py
@@ -68,9 +68,9 @@ class ImplicitQLearning(nn.Module):
         # print(qs.shape)
         # print(targets.shape)
         q_loss = F.mse_loss(qs.float(), targets.float())
-        with open('loss_file.txt', 'a') as f:
-            f.write("q_loss is"+str(q_loss))
-            f.write('\n')
+        # with open('loss_file.txt', 'a') as f:
+        #     f.write("q_loss is"+str(q_loss))
+        #     f.write('\n')
         self.q_optimizer.zero_grad(set_to_none=True)
         q_loss.backward()
         self.q_optimizer.step()
@@ -87,7 +87,17 @@ class ImplicitQLearning(nn.Module):
         q_value = list(self.q_target(task_ids, free_ids, inv_ids, obs_ids, action_ids))
         lst = [i for i in zip(q_value, actions)]
         lst.sort(reverse=True)
-        return lst[0][1], lst[0][0]
+        return lst[0][1]
+    
+    def get_q(self, taskDes, freelook, inv, observation, actions):
+        task_ids = [self.sp.EncodeAsIds(taskDes) for i in range(len(actions))]
+        free_ids = [self.sp.EncodeAsIds(freelook) for i in range(len(actions))]
+        inv_ids = [self.sp.EncodeAsIds(inv) for i in range(len(actions))]
+        obs_ids = [self.sp.EncodeAsIds(observation) for i in range(len(actions))]
+        action_ids = [self.sp.EncodeAsIds(action) for action in actions]
+        q_value = self.q_target(task_ids, free_ids, inv_ids, obs_ids, action_ids)
+        v_value = self.vf(task_ids, free_ids, inv_ids, obs_ids)
+        return q_value-v_value
     
     
 class ImplicitQLearning_webshop(nn.Module):
@@ -95,20 +105,17 @@ class ImplicitQLearning_webshop(nn.Module):
         super().__init__()
         self.sp = spm.SentencePieceProcessor()
         self.sp.Load(args.spm_path)
-        # self.qf = DRRN_Q_web(len(self.sp), args.embedding_dim, args.hidden_dim).to(DEFAULT_DEVICE)
         self.qf = DRRN_TwinQ(len(self.sp), args.embedding_dim, args.hidden_dim).to(DEFAULT_DEVICE)
         self.q_target = copy.deepcopy(self.qf).requires_grad_(False).to(DEFAULT_DEVICE)
         self.vf = DRRN_V_web(len(self.sp), args.embedding_dim, args.hidden_dim).to(DEFAULT_DEVICE)
-        # self.policy = DRRN_TwinQ(len(self.sp), args.embedding_dim, args.hidden_dim).to(DEFAULT_DEVICE)
         self.v_optimizer = optimizer_factory(self.vf.parameters())
         self.q_optimizer = optimizer_factory(self.qf.parameters())
-        # self.policy_optimizer = optimizer_factory(self.policy.parameters())
         self.tau = args.tau
         self.beta = args.beta
         self.discount = args.discount
         self.alpha = args.alpha
 
-    def update(self,observations, taskDes, actions, next_observations, rewards, terminals):
+    def update(self,observations, taskDes,  actions, next_observations, rewards, terminals):
         obs_ids = [self.sp.EncodeAsIds(o) for o in observations]
         task_ids = [self.sp.EncodeAsIds(t) for t in taskDes]
         # TextWorld
@@ -127,8 +134,6 @@ class ImplicitQLearning_webshop(nn.Module):
         v = self.vf(task_ids, obs_ids)
         adv = target_q - v
         v_loss = asymmetric_l2_loss(adv, self.tau)
-        with open('loss_file.txt', 'a') as f:
-            f.write("v_loss is"+str(v_loss))
         self.v_optimizer.zero_grad(set_to_none=True)
         v_loss.backward()
         self.v_optimizer.step()
@@ -141,10 +146,7 @@ class ImplicitQLearning_webshop(nn.Module):
         qs = self.qf(task_ids, obs_ids, action_ids)
         # print(qs.shape)
         # print(targets.shape)
-        q_loss = F.mse_loss(qs.float(), targets.float())
-        with open('loss_file.txt', 'a') as f:
-            f.write("q_loss is"+str(q_loss))
-            f.write('\n')
+        q_loss = F.mse_loss(qs, targets)
         self.q_optimizer.zero_grad(set_to_none=True)
         q_loss.backward()
         self.q_optimizer.step()
@@ -159,4 +161,13 @@ class ImplicitQLearning_webshop(nn.Module):
         q_value = list(self.q_target(task_ids, obs_ids, action_ids))
         lst = [i for i in zip(q_value, actions)]
         lst.sort(reverse=True)
-        return lst[0][1], lst[0][0]
+        return lst[0][1]
+    
+    def get_q(self, taskDes, observation, actions):
+        task_ids = [self.sp.EncodeAsIds(taskDes) for i in range(len(actions))]
+        obs_ids = [self.sp.EncodeAsIds(observation) for i in range(len(actions))]
+        action_ids = [self.sp.EncodeAsIds(action) for action in actions]
+        with torch.no_grad():
+            q_value = self.q_target(task_ids, obs_ids, action_ids)
+            v_value = self.vf(task_ids, obs_ids)
+        return q_value-v_value
